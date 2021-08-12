894526c81e3c580ad10dd43d823006b25480a912
Seona Bellamy <seona13@gmail.com>
2021-08-13 09:35:06 +1000

    Player Movement - Animation
    * Added an Idle animation to the Player.
    
    * Added transitions and associated code to switch to a Walk animation when player is moving, then back to Idle when they stop.
    
    * Tweaked NavMeshAgent values so Darren stops looking like he's sliding.

--- Assets/The Great Fleece/Game/Scripts/Player.cs
+++ Assets/The Great Fleece/Game/Scripts/Player.cs
@@ -7,6 +7,8 @@ using UnityEngine.AI;
 public class Player : MonoBehaviour
 {
 	private NavMeshAgent _agent;
+	private Animator _anim;
+	private Vector3 _destination;
 
 
 
@@ -16,6 +18,12 @@ public class Player : MonoBehaviour
 		if (_agent == null)
 		{
 			Debug.LogError("Player missing NavMeshAgent.");
+		}
+
+		_anim = GetComponentInChildren<Animator>();
+		if (_anim == null)
+		{
+			Debug.LogError("Player missing Animator on model.");
 		}
 	}
 
@@ -29,8 +37,17 @@ public class Player : MonoBehaviour
 			if (Physics.Raycast(ray, out RaycastHit hitInfo))
 			{
 				//Debug.Log("Hit at: (" + Input.mousePosition.x + ", " + Input.mousePosition.y + ", " + Input.mousePosition.z + ")");
-				_agent.SetDestination(hitInfo.point);
+				_destination = hitInfo.point;
+				_agent.SetDestination(_destination);
+				_anim.SetBool("isWalking", true);
 			}
+		}
+
+		float distance = Vector3.Distance(transform.position, _destination);
+
+		if (distance < 1.0f)
+		{
+			_anim.SetBool("isWalking", false);
 		}
 	}
 }
