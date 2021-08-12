1a7b70ea2532d84b66d28809f5d1b146cd23b379
Seona Bellamy <seona13@gmail.com>
2021-08-13 06:31:43 +1000

    Player Movement - Move to Click
    * Move the Player capsule to the point clicked on using the NavMeshAgent.
    
    * Re-baked the navmesh after turning all the scenery static, so that the AI stops walking through things.
    
    * Removed the test HitMarker sphere and its associated code.

--- Assets/The Great Fleece/Game/Scripts/Player.cs
+++ Assets/The Great Fleece/Game/Scripts/Player.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 
 public class Player : MonoBehaviour
 {
-	[SerializeField]
-	private GameObject _hitMarker;
+	private NavMeshAgent _agent;
 
 
 
 	void Start()
 	{
-
+		_agent = GetComponent<NavMeshAgent>();
+		if (_agent == null)
+		{
+			Debug.LogError("Player missing NavMeshAgent.");
+		}
 	}
 
 
@@ -24,8 +28,8 @@ public class Player : MonoBehaviour
 
 			if (Physics.Raycast(ray, out RaycastHit hitInfo))
 			{
-				Debug.Log("Hit at: (" + Input.mousePosition.x + ", " + Input.mousePosition.y + ", " + Input.mousePosition.z + ")");
-				_hitMarker.transform.position = new Vector3(hitInfo.point.x, -2, hitInfo.point.z);
+				//Debug.Log("Hit at: (" + Input.mousePosition.x + ", " + Input.mousePosition.y + ", " + Input.mousePosition.z + ")");
+				_agent.SetDestination(hitInfo.point);
 			}
 		}
 	}
