c5c734da57793685a2f5cc4bc027edd7ab99987c
Seona Bellamy <seona13@gmail.com>
2021-08-14 06:37:10 +1000

    Guard AI - Initial Setup
    * Gave each guard a NavMeshAgent and an AI script.
    
    * Set up waypoints for the first guard to move between.

---
+++ Assets/The Great Fleece/Game/Scripts/GuardAI.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class GuardAI : MonoBehaviour
+{
+	[SerializeField]
+	private List<Transform> _waypoints;
+
+
+
+	void Start()
+	{
+
+	}
+
+
+	void Update()
+	{
+
+	}
+}
