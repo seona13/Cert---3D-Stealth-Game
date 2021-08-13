f6277410cc1bfa2afecd124c608ca5360d094cbe
Seona Bellamy <seona13@gmail.com>
2021-08-14 09:09:11 +1000

    Guard AI - Pausing Top and Tail
    * Pause the guard for 2-5 seconds at the start and end of its route.

--- Assets/The Great Fleece/Game/Scripts/GuardAI.cs
+++ Assets/The Great Fleece/Game/Scripts/GuardAI.cs
@@ -9,8 +9,10 @@ public class GuardAI : MonoBehaviour
 	[SerializeField]
 	private List<Transform> _waypoints;
 	private NavMeshAgent _agent;
+	[SerializeField]
 	private int _currentTarget;
-	private bool _reverse = false;
+	private bool _reverse;
+	private bool _targetReached;
 
 
 
@@ -32,29 +34,48 @@ public class GuardAI : MonoBehaviour
 
 			float distance = Vector3.Distance(transform.position, _waypoints[_currentTarget].position);
 
-			if (distance < 1f)
+			if (distance < 1f && _targetReached == false)
 			{
-				if (_reverse)
-				{
-					_currentTarget--;
+				_targetReached = true;
 
-					if (_currentTarget < 0)
-					{
-						_reverse = false;
-						_currentTarget++;
-					}
-				}
-				else
-				{
-					_currentTarget++;
-
-					if (_currentTarget == _waypoints.Count)
-					{
-						_reverse = true;
-						_currentTarget--;
-					}
-				}
+				StartCoroutine(WaitBeforeMoving());
 			}
 		}
+	}
+
+
+	IEnumerator WaitBeforeMoving()
+	{
+		if (_currentTarget == 0 || _currentTarget == _waypoints.Count - 1)
+		{
+			yield return new WaitForSeconds(Random.Range(2f, 5f));
+		}
+		else
+		{
+			yield return null;
+		}
+
+		if (_reverse)
+		{
+			_currentTarget--;
+
+			if (_currentTarget < 0)
+			{
+				_reverse = false;
+				_currentTarget++;
+			}
+		}
+		else
+		{
+			_currentTarget++;
+
+			if (_currentTarget == _waypoints.Count)
+			{
+				_reverse = true;
+				_currentTarget--;
+			}
+		}
+
+		_targetReached = false;
 	}
 }
