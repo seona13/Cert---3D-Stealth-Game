e19541faa7c0bc7aa33e3c631f68547d6f95370f
Seona Bellamy <seona13@gmail.com>
2021-08-14 06:58:04 +1000

    Guard AI - Moving to First Waypoint
    * Check if the guard has waypoints assigned, and if so use the NavMeshAgent to move to the first one.

--- Assets/The Great Fleece/Game/Scripts/GuardAI.cs
+++ Assets/The Great Fleece/Game/Scripts/GuardAI.cs
@@ -1,23 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 
 public class GuardAI : MonoBehaviour
 {
 	[SerializeField]
 	private List<Transform> _waypoints;
+	private Transform _currentTarget;
+	private NavMeshAgent _agent;
 
 
 
 	void Start()
 	{
+		_agent = GetComponent<NavMeshAgent>();
+		if (_agent == null)
+		{
+			Debug.LogError("Security Guard missing NavMeshAgent.");
+		}
 
+		if (_waypoints.Count > 0 && _waypoints[0] != null)
+		{
+			_currentTarget = _waypoints[0];
+		}
 	}
 
 
 	void Update()
 	{
+		if (_currentTarget != null)
+		{
+			float distance = Vector3.Distance(transform.position, _currentTarget.position);
 
+			if (distance > 1f)
+			{
+				_agent.SetDestination(_currentTarget.position);
+			}
+		}
 	}
 }
