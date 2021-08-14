c0cb6138c060564bb7fb88c225d572e8e03d9497
Seona Bellamy <seona13@gmail.com>
2021-08-15 09:21:34 +1000

    Security Cameras - Pause Camera Rotation
    * When Darren is detected by the camera, stop it rotating and pause of half a second before starting the cut-scene.

--- Assets/The Great Fleece/Game/Scripts/SecurityCamSight.cs
+++ Assets/The Great Fleece/Game/Scripts/SecurityCamSight.cs
@@ -5,16 +5,35 @@ using UnityEngine;
 
 public class SecurityCamSight : MonoBehaviour
 {
+	private Animator _anim;
 	[SerializeField]
 	private GameObject _cutscene;
 
+
+
+	private void Start()
+	{
+		_anim = GetComponentInParent<Animator>();
+		if (_anim == null)
+		{
+			Debug.LogError("Security Camera missing Animator.");
+		}
+	}
 
 
 	void OnTriggerEnter(Collider other)
 	{
 		if (other.CompareTag("Player"))
 		{
-			_cutscene.SetActive(true);
+			_anim.enabled = false;
+			StartCoroutine(PauseCamera());
 		}
+	}
+
+
+	IEnumerator PauseCamera()
+	{
+		yield return new WaitForSeconds(0.5f);
+		_cutscene.SetActive(true);
 	}
 }
